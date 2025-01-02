ba9e142079a237dfc01ca51b503ca6f6e9d3c432
ChaviduBandara <152202463+ChaviduBandara@users.noreply.github.com>
2025-01-02 22:37:25 +0530

    updated code

--- src/main/java/edu/icet/contoller/StudentController.java
+++ src/main/java/edu/icet/contoller/StudentController.java
@@ -1,13 +1,18 @@
 package edu.icet.contoller;
 
+import edu.icet.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController     // must put this annotation if putting a network request (get,post,etc)
 public class StudentController {
 
+    @Autowired
+    StudentService service;
+
     @GetMapping("/get")
     public String getStudent(){
-        return "Saman";
+        return service.getStudent();
     }
 }
