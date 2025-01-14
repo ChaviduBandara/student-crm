b8596dc3c2215f07280ae55fa8b5aa8b4d46a9f4
ChaviduBandara <152202463+ChaviduBandara@users.noreply.github.com>
2025-01-14 22:02:35 +0530

    updated service class

--- src/main/java/edu/icet/contoller/StudentController.java
+++ src/main/java/edu/icet/contoller/StudentController.java
@@ -4,6 +4,8 @@ import edu.icet.dto.Student;
 import edu.icet.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,9 +16,14 @@ public class StudentController {
     @Autowired
     StudentService service;     // student service kiyana interface eken object ekak hadanna bane, eka nisa spring boot interface eka implements karapu sub class eka hoyagana gihilla eken object ekak handala denwa
 
-    @GetMapping("/")
-    public List<Student> getStudents(){
+    @GetMapping
+    public List<Student> getStudents() {
         return service.getStudent();
+    }
+
+    @PostMapping
+    public void addStudent(@RequestBody Student student){
+        service.addStudent(student);
     }
 
 
--- src/main/java/edu/icet/service/StudentServiceImpl.java
+++ src/main/java/edu/icet/service/StudentServiceImpl.java
@@ -8,19 +8,20 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
-@Service
-public class StudentServiceImpl implements StudentService{
+@Service    // service is the class where the business implementation is written
+public class StudentServiceImpl implements StudentService {
 
+    @Autowired
     StudentRepository repository;
 
 
     // ** Doing a CRUD with the DB **
     @Override
     public List<Student> getStudent() {    //To print the students from the DB (reading)
-        List<Student> all = repository.findAll();   // findAll() => SELECT * FROM student
+        List<Student> all = repository.findAll();   // findAll() => SELECT * FROM student (all is a local variable of type list)
         return all;
     }
- 
+
     @Override
     public void addStudent(Student student) {   //To add a student to the DB (updating)
         repository.save(student);   // short form of => (INSERT INTO student, VALUES (*****);
